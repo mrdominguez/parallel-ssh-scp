13fb433c8a8fc707eef21f2b63439cd8cab3f785
Mariano Dominguez <marianodominguez@hotmail.com>
2020-06-03 01:00:42 -0400

    Update scpexp.pl

--- scpexp.pl
+++ scpexp.pl
@@ -83,6 +83,9 @@ $scp .= " -r" if $r;
 $scp .= ( $tolocal ? " $username\@$host:$spath $tpath" : " $spath $username\@$host:$tpath" );
 
 my $exp = new Expect;
+$exp->raw_pty(0);
+$exp->log_user(0);		# Set (1) -default-, unset (0) logging to STDOUT
+#$exp->log_file("$0.log","a");	# Log session to file (a=append -default-, w=truncate)
 
 if ( $v ) {
 	print "Source path = $spath\n";
