dc688b154a7b79f67ddb1a542826076af5b3df51
Mariano Dominguez <marianodominguez@hotmail.com>
2020-06-07 10:06:04 -0400

    Update sshexp.pl

--- sshexp.pl
+++ sshexp.pl
@@ -156,7 +156,7 @@ if ( !defined $cmd ) {
 	my $user = $sudo ? $sudo_user : $username;
 	my $msg = '';
 	
-	# Comment out to disable welcome message
+	# Comment out to remove message
 	$msg = "echo -e '#\\n# Connected to $host\\n# Logged in as $user";
 	$msg .= " via sudo (by $username)" if $sudo;
 	$msg .= "\\n#'";
