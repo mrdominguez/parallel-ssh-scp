6e53683ade39a39c872d30153f57a8ee71637775
Mariano Dominguez <marianodominguez@hotmail.com>
2022-02-16 18:52:22 -0500

    Update mdssh.pl

--- mdssh.pl
+++ mdssh.pl
@@ -42,8 +42,8 @@ my $odir_default = $ENV{PWD};
 if ( $version ) {
 	print "Asyncronous parallel SSH/SCP command-line utility\n";
 	print "Author: Mariano Dominguez\n";
-	print "Version: 6.5.2\n";
-	print "Release date: 2022-02-09\n";
+	print "Version: 6.5.3\n";
+	print "Release date: 2022-02-16\n";
 	exit;
 }
 
