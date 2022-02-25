9f0b3c17ed10c13c8c36d09184224949b87e110d
Mariano Dominguez <marianodominguez@hotmail.com>
2022-02-25 14:43:31 -0500

    Update sshexp.pl

--- sshexp.pl
+++ sshexp.pl
@@ -35,8 +35,8 @@ if ( $d ) {
 if ( $version ) {
 	print "SSH command-line utility\n";
 	print "Author: Mariano Dominguez\n";
-	print "Version: 6.5.3\n";
-	print "Release date: 2022-02-16\n";
+	print "Version: 6.6\n";
+	print "Release date: 2022-02-25\n";
 	exit;
 }
 
