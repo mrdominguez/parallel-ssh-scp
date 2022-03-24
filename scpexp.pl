61805a00d245f2105e8ce3031c3da306437d61e5
Mariano Dominguez <marianodominguez@hotmail.com>
2022-03-24 12:31:46 -0400

    Update scpexp.pl

--- scpexp.pl
+++ scpexp.pl
@@ -36,8 +36,8 @@ if ( $d ) {
 if ( $version ) {
 	print "SCP command-line utility\n";
 	print "Author: Mariano Dominguez\n";
-	print "Version: 6.6\n";
-	print "Release date: 2022-02-25\n";
+	print "Version: 6.6.1\n";
+	print "Release date: 2022-03-24\n";
 	exit;
 }
 
@@ -210,13 +210,12 @@ sub usage {
 
 	print "\t -help : Display usage\n";
 	print "\t -version : Display version information\n";
-	print "\t -u : Username (default: \$USER -current user-, ignored when using -via or Okta credentials)\n";
+	print "\t -u : Username (default: \$USER -current user-, ignored when using Okta credentials -sft login-)\n";
 	print "\t -p : Password or path to password file (default: undef)\n";
 	print "\t -via : Bastion host for Okta ASA sft client (default over -proxy)\n";
 	print "\t -proxy : Proxy host for ProxyJump (leave empty to enable over -via)\n";
 	print "\t   -bu : Bastion user\n";
 	print "\t   -ru : Remote user\n";
-	print "\t         (default: Okta username -sft login-)\n";
 	print "\t -sshOpts : Additional SSH options\n";
 	print "\t            (default: -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null)\n";
 	print "\t            Example: -sshOpts='-o UserKnownHostsFile=/dev/null -o ConnectTimeout=10'\n";
